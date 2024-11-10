9a9563a6af3843ca8fcdbe61ec736e9a986e5b6c
Bodo8 <b.trojanski@gmail.com>
2024-11-10 11:23:49 +0100

    class par

---
+++ CrudWinFormsMvp/Presenters/ClassPar2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudWinFormsMvp.Presenters
+{
+    internal class ClassPar2
+    {
+        public string Name { get; set; }
+    }
+}
